8d01887d700079babee5e99b8e97671732bc4536
aakash sinha <aksinha2509@gmail.com>
2022-03-04 16:50:10 +0530

    Initial Commit

---
+++ Edvora/EdvoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdvoraApp.swift
+//  Edvora
+//
+//  Created by Aakash Sinha on 04/03/22.
+//
+
+import SwiftUI
+
+@main
+struct EdvoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
